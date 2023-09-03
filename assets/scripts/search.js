1ea5292367de8044250bcf6dbd5fc1152ed99f7e
JonathasSC <jonathas.s.cardoso28@gmaill.com>
2023-09-03 09:24:38 -0300

    feat: search bar style finished

---
+++ assets/scripts/search.js
@@ -0,0 +1,10 @@
+searchInput = document.getElementById("search__input");
+searchClear = document.getElementById("searchbar__clear");
+
+if (searchInput.value !== ""){
+    console.log(searchInput.value)
+};
+
+async () => {
+    
+};
