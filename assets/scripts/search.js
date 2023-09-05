0058ec81b642f4347e8b0459fb71e8bea819f0e9
JonathasSC <jonathas.s.cardoso28@gmaill.com>
2023-09-05 15:41:14 -0300

    feat(search bar): clear search bar finished

--- assets/scripts/search.js
+++ assets/scripts/search.js
@@ -1,4 +1,19 @@
+function viewClearBtn(){
+    const searchClear = document.getElementById("searchbar__clear");
+    const searchInput = document.getElementById("search__input");
+    
+    if(searchInput.value.length > 0){
+        searchClear.style.display = 'block';
+    } else {
+        searchClear.style.display = 'none';
+    }
+}
+
+
 function searchClear(){
-    searchInput = document.getElementById("search__input");
-    searchInput.value = ""
+    const searchClear = document.getElementById("searchbar__clear");
+    const searchInput = document.getElementById("search__input");
+
+    searchInput.value = "";
+    searchClear.style.display = 'none';
 }
